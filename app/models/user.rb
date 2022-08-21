7c0543e5c5adac0d1a2e4a937ebebaf8f9d15fe1
Luiz Gustavo <l.cgoncalvea@gmail.com>
2022-08-21 20:01:30 -0300

    :bug: fix: user_role_change

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ has_one:institution
 validates :user_type, presence: true
 validate -> { errors.add(:user_type, "Tipo nao valido") unless ["Student", "Institution"].include?(user_type)}
 
-before_save :set_user_role
+before_create :set_user_role
 
 def set_user_role
   if user_type.downcase == "student" then self.roles = ["STUDENT"] end
