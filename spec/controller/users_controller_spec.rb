6e6f982fe7077b3977015032850d6c1e0b557f80
LuizGGoncalves <l.cgoncalvea@gmail.com>
2023-02-22 15:15:16 -0300

    :test_tube: test: UseCase and user_controller

---
+++ spec/controller/users_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #self_info' do
+    context 'when user is signed in' do
+      let(:user) { create(:user) }
+      let!(:student) { create(:student, user: user) }
+
+      before do
+        sign_in user
+        allow(controller).to receive(:current_user).and_return(user)
+        get :self_info
+      end
+
+      it 'Should return status 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'Should return user info in JSON format' do
+        expect(response.content_type).to eq 'application/json; charset=utf-8'
+        expect(JSON.parse(response.body)["nome"]).to eq('testeStudent')
+      end
+    end
+
+    context 'when user is not logged in' do
+      before do
+        get :self_info
+      end
+
+      it "returs not autorized" do
+        expect(response.status).to eq 400
+      end
+    end
+  end
+end
---
+++ spec/use_case/use_case_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe UseCase do
+
+  describe ".perform" do
+    class Dummy
+      include UseCase
+
+      def initialize(teste)
+        @teste = teste
+      end
+    end
+    context "when peform is implemented" do
+      it "include UseCase, generate a new instance with arguments and perform it" do
+        args = { teste: "laranja" }
+        use_case = instance_double(Dummy, perform:true)
+        allow(Dummy).to receive(:new).with(args).and_return(use_case)
+
+        expect(Dummy.perform(args)).to be_truthy
+        expect(Dummy).to have_received(:new).with(args)
+        expect(use_case).to have_received(:perform)
+      end
+    end
+
+    context "when perform is not implemented" do
+      it 'raises a NotImplementedError' do
+        use_case = Dummy.new({})
+        expect { use_case.perform }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+end
